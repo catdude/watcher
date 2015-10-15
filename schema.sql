d894e4b11bf54d9eb684ae49da3b1dc43a5a47ec
Dan Mahoney <catdude@gmail.com>
2015-10-15 16:49:07 -0500

    Cleaned up and expanded schema layout for SQLite3. Should allow for actual practical reading and processing SNMP values.

--- schema.sql
+++ schema.sql
@@ -1,46 +1,61 @@
-CREATE TABLE measurement (  -- Description of measurements to be performed
-    id INTEGER PRIMAY KEY,
-    name V  name VARCHAR(32) NOT NULL,
-    class INTEGER NOT NULL,
-    db VARCHAR(64) NOT NULL,
-    desc VARCHAR(64),
-    period INTEGER
+CREATE TABLE measurement (      -- Description of measurements to be performed
+    measID INTEGER PRIMAY KEY,
+    name VARCHAR(32) NOT NULL,  -- Name of this measurement
+    class INTEGER NOT NULL,     -- Foreign key link to class.id
+    db VARCHAR(64) NOT NULL,    -- Name of Influxdb database 
+    desc VARCHAR(64),           -- Text description of the measurements
+    period INTEGER              -- How often should test tun (minutes)
 );
-CREATE TABLE class (        -- Device class (i.e. DNCC, CDS, Linux, CiscoSwitch, etc)
-    id INTEGER PRIMARY KEY,
+
+CREATE TABLE class (            -- Device class (i.e. DNCC, CDS, Linux, CiscoSwitch, etc)
+    classID INTEGER PRIMARY KEY,
     name VARCHAR(32) NOT NULL
 );
-CREATE TABLE collector (    -- All hosts that will be collecting data from devices for Watcher
-    id INTEGER PRIMARY KEY,
+
+CREATE TABLE collector (        -- All hosts that will be collecting data from devices for Watcher
+    collID INTEGER PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     ip VARCHAR(16) NOT NULL
 );
-CREATE TABLE site (     -- Locations where devices are clustered (NLV, Detroit, EU, Lux, etc)
-    id INTEGER PRIMARY KEY,
+
+CREATE TABLE site (             -- Locations where devices are clustered (NLV, Detroit, EU, Lux, etc)
+    siteID INTEGER PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
-    thru INTEGER,       -- If connected through VPN, through which site does VPN run
-    collector INTEGER
+    thru INTEGER,               -- If connected through VPN, through which site does VPN run
+    collector INTEGER           -- Foreign key link to collector.id
 );
-CREATE TABLE oid  (
-id INTEGER PRIMARY KEY,
-measurement INTEGER,        -- With which measurement is this OID associated?
-oid VARCHAR(128),
-indexOid INTEGER,           -- If this OID is indexed, what is the numeric OID to the index?
-top INTEGER,                -- Is this the top of a subtree? If so, how many octets are index values?
-name VARCHAR(128),
-class INTEGER,              -- Which device class does this apply to? Default is ALL.
-mib VARCHAR(32),
-desc TEXT,
-type VARCHAR(32),
-scanner INTEGER             -- Which data scanner will evaluate the return and stuff into Influxdb?
+
+CREATE TABLE host (
+    hostID INTEGER PRIMARY KEY,
+    name CHAR(64),
+    ip CHAR1(16),
+    class INTEGER,              -- Foreign  key link to class.classID (DNCC, CDS, etc)
+    site INTEGER,               -- Foreign key link to site.id
+    status INTEGER,             -- 0 = inactive, 1 = active
+    type INTEGER,               -- 0 = ping only, 1 = SNMP
+    community CHAR(32)          -- SNMP community string, if applicable
 );
-CREATE TABLE hosts (
-id INTEGER PRIMARY KEY,
-name CHAR(64),
-ip CHAR1(16),
-class INTEGER,     -- Foreign  key link to "class" table (DNCC, CDS, etc)
-site INTEGER,
-status INTEGER,      -- 0 = inactive, 1 = active
-type INTEGER,       -- 0 = ping only, 1 = SNMP
-community CHAR(32) -- SNMP community string, if applicable
+
+CREATE TABLE test_master (      --  Single-line OIDs and the top (table) entry for table-based OIDs
+    masterID INTEGER PRIMARY KEY,
+    oid CHAR(128),              -- Numeric OID for single-line and top-of-table (entry) OIDs
+    name CHAR(128),             -- Text name (optional)
+    mib CHAR(64),               -- Name of MIB defining OID (optional)
+    class INTEGER,              -- Might be deleted later - class of item being read
+    measurement INTEGER         -- Foreign key to measurement.measID
 );
+
+CREATE TABLE test_item (        -- single-line OIDs and line-item OIDs from above table-based OIDs
+    itemID INTEGER PRIMARY KEY,
+    oid CHAR(128),
+    measurement INTEGER,        -- Foreign key link to measurement.measID
+    series CHAR(64)             -- Name of InfluxDB series to store values
+);
+
+CREATE TABLE test_parse (       -- Indicates how many levels deep indexes go for table-based OIDS
+    parseID INTEGER PRIMARY KEY,
+    oid INTEGER,                -- Foreign Key link to test_item.id
+    levels INTEGER,             -- Number of levels indexes are nested
+    measurement INTEGER         -- Foreign key link to measurement.measID
+);
+
